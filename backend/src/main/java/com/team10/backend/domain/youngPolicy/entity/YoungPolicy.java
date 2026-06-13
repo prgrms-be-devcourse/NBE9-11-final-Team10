b5ee201beb3a0aa0be5a87be4da6adc04b53001b
DonggunSe0 <cloudhenry1@gmail.com>
2026-06-13 20:21:32 +0900

    fix: YoungPolicy entity 식별자 추가

--- backend/src/main/java/com/team10/backend/domain/youngPolicy/entity/YoungPolicy.java
+++ backend/src/main/java/com/team10/backend/domain/youngPolicy/entity/YoungPolicy.java
@@ -1,7 +1,19 @@
 package com.team10.backend.domain.youngPolicy.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class YoungPolicy {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 번호를 1부터 자동으로 1씩 증가시킵니다.
+    private Long id;
 }
--- backend/src/main/java/com/team10/backend/domain/youngPolicy/exception/YoungPolicyErrorCode.java
+++ backend/src/main/java/com/team10/backend/domain/youngPolicy/exception/YoungPolicyErrorCode.java
@@ -1,7 +1,4 @@
 package com.team10.backend.domain.youngPolicy.exception;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
 public enum YoungPolicyErrorCode {
 }
