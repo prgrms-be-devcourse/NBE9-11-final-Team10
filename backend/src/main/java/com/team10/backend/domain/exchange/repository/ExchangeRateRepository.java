2c48669a689274c2c036bc68c3291e45e1a6378c
perandowind <perandowind@gmail.com>
2026-06-13 17:11:47 +0900

    feature: repository, controller 엔드포인트 정의

--- backend/src/main/java/com/team10/backend/domain/exchange/repository/ExchangeRateRepository.java
+++ backend/src/main/java/com/team10/backend/domain/exchange/repository/ExchangeRateRepository.java
@@ -1,4 +1,7 @@
 package com.team10.backend.domain.exchange.repository;
 
-public interface ExchangeRateRepository {
+import com.team10.backend.domain.exchange.entity.ExchangeRate;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExchangeRateRepository extends JpaRepository<ExchangeRate, Long> {
 }
