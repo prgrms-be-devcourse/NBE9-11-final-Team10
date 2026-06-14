09f1cae498c2b25dc4bc0d5bacaf7b19b6e091d5
DonggunSe0 <cloudhenry1@gmail.com>
2026-06-14 15:20:37 +0900

    feature: api Req, Res Dto 구현

--- backend/src/main/java/com/team10/backend/domain/youngPolicy/dto/req/YoungPolicyReq.java
+++ backend/src/main/java/com/team10/backend/domain/youngPolicy/dto/req/YoungPolicyReq.java
@@ -1,4 +1,9 @@
 package com.team10.backend.domain.youngPolicy.dto.req;
 
-public record YoungPolicyReq() {
+public record YoungPolicyReq(
+        String apiKeyNm, // api 인증키
+        Integer pageNum,
+        Integer pageSize
+        // 필요한 검색 조건 이후에 추가
+) {
 }
--- backend/src/main/java/com/team10/backend/domain/youngPolicy/dto/res/YoungPolicyRes.java
+++ backend/src/main/java/com/team10/backend/domain/youngPolicy/dto/res/YoungPolicyRes.java
@@ -1,4 +1,24 @@
 package com.team10.backend.domain.youngPolicy.dto.res;
 
-public record YoungPolicyRes() {
+import java.util.List;
+
+public record YoungPolicyRes(
+        List<PolicyItem> youthPolicyList // 청년 정책 리스트
+) {
+    // API에서 받아올 핵심 항목들만 선언합니다.
+    public record PolicyItem(
+            String plcyNo,         // 정책번호
+            String plcyNm,         // 정책명
+            String plcyExplnCn, //정책 상세한 설명
+            String lclsfNm, // 정책 카테고리 대분류
+            String mclsfNm, // 정책카 카테고리 중분류
+            String sprtTrgtMinAge, // 최소연령
+            String sprtTrgtMaxAge,  // 최대연령
+            String zipCd, // 정책거주지역 코드
+            String jobCd, // 정책 취업요건 코드
+            String aplyYmd, // 신청기간
+            String aplyUrlAddr, // 신청URL 주소
+            String plcyAplyMthdCn //정책신청 방법 설명
+    ) {
+    }
 }
