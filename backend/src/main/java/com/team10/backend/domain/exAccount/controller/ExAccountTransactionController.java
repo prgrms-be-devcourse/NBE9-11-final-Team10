cec99572ccfa8b2da67c34b5ba50e6f2a353eee7
DonggunSe0 <cloudhenry1@gmail.com>
2026-06-18 16:02:54 +0900

    Feature/work 51 1.extrernal assets domin contract (#63)
    * chore: externalAccount domain 생성
    
    * feature: 계좌 상태 타입 구분및 Res 구현
    
    * fix: req record로 수정
    
    * feature: 외부계좌 호출 api 골결 구조 구현

---
+++ backend/src/main/java/com/team10/backend/domain/exAccount/controller/ExAccountTransactionController.java
@@ -0,0 +1,9 @@
+package com.team10.backend.domain.exAccount.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Tag(name = "External Account Transaction", description = "외부 계좌 거래내역 API")
+public class ExAccountTransactionController {
+}
---
+++ backend/src/main/java/com/team10/backend/domain/exAccount/exception/ExAccountErrorCode.java
@@ -0,0 +1,7 @@
+package com.team10.backend.domain.exAccount.exception;
+
+import com.team10.backend.global.exception.ErrorCode;
+
+public class ExAccountErrorCode {
+
+}
