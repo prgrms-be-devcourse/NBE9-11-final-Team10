7f5ad32a2678f490d39b3e86ce6a9795055847cc
DonggunSe0 <cloudhenry1@gmail.com>
2026-06-19 09:50:21 +0900

    feature: exAccount basic Controller 구현

--- backend/src/main/java/com/team10/backend/domain/exAccount/controller/ExAccountController.java
+++ backend/src/main/java/com/team10/backend/domain/exAccount/controller/ExAccountController.java
@@ -1,13 +1,30 @@
 package com.team10.backend.domain.exAccount.controller;
 
+import com.team10.backend.domain.exAccount.dto.res.ExAccountRes;
+import com.team10.backend.domain.exAccount.service.ExAccountService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/external-accounts")
 @Tag(name = "External Account", description = "외부 계좌 API")
 public class ExAccountController {
+    private final ExAccountService exAccountService;
+
+    @GetMapping("/accounts")
+    public ResponseEntity<List<ExAccountRes>> getAccounts(@AuthenticationPrincipal Long userId) {
+        return ResponseEntity.ok(exAccountService.getAccounts(userId));
+    }
+
+
+
+
 }
--- backend/src/main/java/com/team10/backend/domain/exAccount/controller/ExAccountTransactionController.java
+++ backend/src/main/java/com/team10/backend/domain/exAccount/controller/ExAccountTransactionController.java
@@ -1,9 +1,26 @@
 package com.team10.backend.domain.exAccount.controller;
 
+import com.team10.backend.domain.exAccount.dto.res.ExAccountTransactionRes;
+import com.team10.backend.domain.exAccount.entity.ExAccountTransaction;
+import com.team10.backend.domain.exAccount.repository.ExAccountTransactionRepository;
+import com.team10.backend.domain.exAccount.service.ExAccountTransactionService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequiredArgsConstructor
 @Tag(name = "External Account Transaction", description = "외부 계좌 거래내역 API")
 public class ExAccountTransactionController {
+    private final ExAccountTransactionService exAccountTransactionService;
+
+    @GetMapping("/transactions")
+    public ResponseEntity<List<ExAccountTransactionRes>> getTransactions(@AuthenticationPrincipal Long userId) {
+        return ResponseEntity.ok(exAccountTransactionService.getTransactions(userId));
+    }
 }
