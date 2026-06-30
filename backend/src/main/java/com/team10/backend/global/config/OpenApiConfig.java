273fe54ec6d0614080f3353bd2cb7e9f924ce7a5
JW-1126 <jwyoon1126@naver.com>
2026-06-30 15:03:09 +0900

    docs: swagger 도메인 별 그룹화 적용

--- backend/src/main/java/com/team10/backend/global/config/OpenApiConfig.java
+++ backend/src/main/java/com/team10/backend/global/config/OpenApiConfig.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -26,5 +27,71 @@ public class OpenApiConfig {
                                 .type(SecurityScheme.Type.HTTP)
                                 .scheme("bearer")
                                 .bearerFormat("JWT")));
+    }
+
+    @Bean
+    public GroupedOpenApi authUserApi() {
+        return domainGroup("auth-user",
+                "com.team10.backend.domain.user.controller");
+    }
+
+    @Bean
+    public GroupedOpenApi accountApi() {
+        return domainGroup("accounts",
+                "com.team10.backend.domain.account.controller");
+    }
+
+    @Bean
+    public GroupedOpenApi transactionApi() {
+        return domainGroup("transactions",
+                "com.team10.backend.domain.transaction.controller");
+    }
+
+    @Bean
+    public GroupedOpenApi externalAccountApi() {
+        return domainGroup("external-accounts",
+                "com.team10.backend.domain.exAccount.controller");
+    }
+
+    @Bean
+    public GroupedOpenApi transferApi() {
+        return domainGroup("transfers",
+                "com.team10.backend.domain.transfer.controller");
+    }
+
+    @Bean
+    public GroupedOpenApi savingApi() {
+        return domainGroup("savings",
+                "com.team10.backend.domain.saving.controller");
+    }
+
+    @Bean
+    public GroupedOpenApi exchangeApi() {
+        return domainGroup("exchange",
+                "com.team10.backend.domain.exchange.controller");
+    }
+
+    @Bean
+    public GroupedOpenApi investmentApi() {
+        return domainGroup("investment",
+                "com.team10.backend.domain.investment.account.controller",
+                "com.team10.backend.domain.investment.portfolio.controller",
+                "com.team10.backend.domain.investment.realtime.controller",
+                "com.team10.backend.domain.investment.stock.controller",
+                "com.team10.backend.domain.investment.trade.controller",
+                "com.team10.backend.domain.investment.watchlist.controller");
+    }
+
+    @Bean
+    public GroupedOpenApi youngPolicyApi() {
+        return domainGroup("youth-policies",
+                "com.team10.backend.domain.youngPolicy.controller");
+    }
+
+    private GroupedOpenApi domainGroup(String group, String... packagesToScan) {
+        return GroupedOpenApi.builder()
+                .group(group)
+                .packagesToScan(packagesToScan)
+                .build();
     }
 }
